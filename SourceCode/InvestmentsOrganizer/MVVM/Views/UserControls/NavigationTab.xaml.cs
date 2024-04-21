61953d866e0ae97bf992351a13ed30cc5e0ca055
Przemysław Szewczak <84459965+Przemoosz@users.noreply.github.com>
2024-04-21 18:55:35 +0200

    Created base view with navigation bar (#1)
    * created main view layout
    
    * added navigation tab
    
    * added general style
    
    * Refactor I
    
    * Refactor II

---
+++ SourceCode/InvestmentsOrganizer/MVVM/Views/UserControls/NavigationTab.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace InvestmentsOrganizer.MVVM.Views.UserControls
+{
+	using System.Windows.Controls;
+
+	public partial class NavigationTab : UserControl
+	{
+		public NavigationTab()
+		{
+			InitializeComponent();
+		}
+	}
+}
